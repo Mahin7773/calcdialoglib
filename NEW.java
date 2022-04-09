f64e9d8f717aea0805734a008edac0388413f76c
Mahin7773 <mahinmtk65@gmail.com>
2022-04-09 16:01:24 +0600

    new java class

---
+++ NEW.java
@@ -0,0 +1,18 @@
+public class NEW {
+    private int value;
+    //constructor
+    NEW(int v)
+        {
+            value=v;
+        }
+    //getter
+    public int getValue()
+        {
+            return value;
+        }
+    //setter
+    public int setValue(int v)
+        {
+            value=v;
+        }
+}
